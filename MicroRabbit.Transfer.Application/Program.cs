c7a5a4ead05b35aa363b138f3fc7498b34e81dd3
Sercan <s.selcuk23@gmail.com>
2019-10-23 21:56:01 +0100

    Nuget pacs db context and congf.s For Transfer Microservice

---
+++ MicroRabbit.Transfer.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroRabbit.Transfer.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
